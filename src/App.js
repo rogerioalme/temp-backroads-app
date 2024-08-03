25c89bfc21a1a36e13a5289ab67733e8285e71a9
Rogerio Domingues <rogeriodomingues@MacBook-Pro-de-Rogerio.local>
2024-08-02 23:31:57 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import About from './components/About'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Services from './components/Services'
+import Tours from './components/Tours'
+import Footer from './Footer'
+
+import { pageLinks } from './data'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  )
+}
+
+export default App
